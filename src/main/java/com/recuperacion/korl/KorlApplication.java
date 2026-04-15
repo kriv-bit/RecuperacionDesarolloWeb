0073721140389a25ed0fe1c89d540a386a749f64
Kevin Orlando Rivera <kevinorlandorivera0@gmail.com>
2026-04-15 07:12:13 -0500

    feat: Initial Spring Boot project setup with tech_solution_korl db

---
+++ src/main/java/com/recuperacion/korl/KorlApplication.java
@@ -0,0 +1,13 @@
+package com.recuperacion.korl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recuperacion/korl/KorlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recuperacion.korl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
